a2a59c273661855cb85697488b3de7b31093a9b3
Aditya Gupta <72199742+Adityagupta1625@users.noreply.github.com>
2022-07-31 22:24:24 +0530

    Update index.js

--- src/State/Reducers/index.js
+++ src/State/Reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from "redux";
 import user from "./auth";
 import hackthon from "./hackthon";
 import team from "./team";
-import userTeam from './userTeam';
+import userTeam from './userteam';
 
 const reducers =
 combineReducers({  user,hackthon,team,userTeam });
